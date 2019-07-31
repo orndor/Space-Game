89619178d9beec04e7e204f8ea1c87a4bf7c7679
Shod <dilshod.sadiev@outlook.com>
2019-07-31 14:26:43 -0400

    Implement Shopping class

--- code/App.cs
+++ code/App.cs
@@ -10,10 +10,24 @@ namespace Space_Game
         void Run()
         {
             Product product = new Product();
-            List<Product> products = new List<Product>();
-            List<Product> inventory = new List<Product>();
-            product.AddProducts(products);
             
+            List<Product> products = new List<Product>();
+            List<Product> inventories = new List<Product>();
+            product.AddProducts(products);
+            Shoping shoping = new Shoping(products);
+
+            shoping.Buy(inventories, shoping.FindPrice("Gold"));
+
+            shoping.Sell(inventories, shoping.FindPrice("Gold"));
+
+            //foreach (Product item in products)
+            //{
+            //    if ((item.ProductName == "Gold") && (item.Planet == Global.currentPlanet))
+            //    {
+            //        inventory.Buy(inventories, item);
+            //    }
+            //}
+
             //Product EarthGold = new Product("Earth Gold", 100);
             //Inventory.Products.Add(EarthGold);
 
