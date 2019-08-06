cad5efdd62291888e6b8627e0a79f3fcd747cd69
LUke <porterl2268@student.faytechcc.edu>
2019-08-06 09:03:58 -0400

    .net core 3.0

---
+++ code/Bowser.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Media;
+
+namespace Space_Game
+{
+    class Bowser
+    {
+        public static void PlayMusic()
+        {
+            System.Media.SoundPlayer player = new System.Media.SoundPlayer(@"c:\mywavfile.wav");
+            player.Play();
+
+        }
+    }
+}
